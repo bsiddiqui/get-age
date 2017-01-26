b093fa87d1555f133b0e5173ebfb26acb90b14ab
Basil Siddiqui <basil.siddiqui@gmail.com>
2017-01-26 11:37:53 -0800

    Fix: use UTC to prevent off by 1

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 module.exports = function getAge (dateString) {
   var today = new Date()
   var birthDate = new Date(dateString)
-  var age = today.getFullYear() - birthDate.getFullYear()
-  var month = today.getMonth() - birthDate.getMonth()
-  if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+  var age = today.getUTCFullYear() - birthDate.getUTCFullYear()
+  var month = today.getUTCMonth() - birthDate.getUTCMonth()
+  if (month < 0 || (month === 0 && today.getUTCDate() < birthDate.getUTCDate())) {
     age--
   }
   return age
